332597395d4b72bc3b17478124c59e7b8fce63a0
June Castillote <june.castillote@gmail.com>
2024-07-17 11:44:43 +0800

    Create Send-WindowsServiceReportByTeamsWebhook.ps1

---
+++ source/public/Send-WindowsServiceReportByTeamsWebhook.ps1
@@ -0,0 +1,13 @@
+Function Send-WindowsServiceReportByTeamsWebhook {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        $InputObject,
+
+        [Parameter(Mandatory)]
+        [string]
+        $WebHookUrl
+    )
+
+
+}
