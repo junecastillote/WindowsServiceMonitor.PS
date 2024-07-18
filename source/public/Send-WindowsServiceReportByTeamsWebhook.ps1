201169c0d4d7ea8640a0e0c61efd4fd6bb913fe3
June Castillote <june.castillote@gmail.com>
2024-07-18 11:05:44 +0800

    Update Send-WindowsServiceReportByTeamsWebhook.ps1

--- source/public/Send-WindowsServiceReportByTeamsWebhook.ps1
+++ source/public/Send-WindowsServiceReportByTeamsWebhook.ps1
@@ -2,12 +2,30 @@ Function Send-WindowsServiceReportByTeamsWebhook {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]
+        [PSTypeNameAttribute('WindowsServiceMonitorResult')]
         $InputObject,
 
         [Parameter(Mandatory)]
-        [string]
-        $WebHookUrl
+        [string[]]
+        $TeamsWebHookURL
     )
 
+    $teamsAdaptiveCard = New-WindowsServiceStatusCard -InputObject $InputObject
 
+
+    foreach ($url in $TeamsWebHookURL) {
+        SayInfo "Posting alert to Teams with URL [$($url)]"
+        $Params = @{
+            "URI"         = $url
+            "Method"      = 'POST'
+            "Body"        = $teamsAdaptiveCard | ConvertTo-Json -Depth 10
+            "ContentType" = 'application/json'
+        }
+        try {
+            Invoke-RestMethod @Params -ErrorAction Stop
+        }
+        catch {
+            SayError "Failed to post to channel. $($_.Exception.Message)"
+        }
+    }
 }
